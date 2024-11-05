5251d37daa1d3e787ac1de19ad504ef72814c28d
Dex <kvngalpha000@gmail.com>
2024-11-05 13:47:29 +0100

    Landing page
    l

---
+++ assets/js/script.js
@@ -0,0 +1,235 @@
+(function () {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Toggle .header-reduce
+   */
+  let selectHNavbar = select(".header");
+  if (selectHNavbar) {
+    onscroll(document, () => {
+      if (window.scrollY > 100) {
+        selectHNavbar.classList.add("header-reduce");
+        selectHNavbar.classList.remove("header-trans");
+      } else {
+        selectHNavbar.classList.remove("header-reduce");
+        selectHNavbar.classList.add("header-trans");
+      }
+    });
+  }
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  }
+  window.addEventListener("load", aosInit);
+
+  /**
+   * Testimonials slider
+   */
+  new Swiper(".testimonials-slider", {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    slidesPerView: "auto",
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
+  });
+
+  /**
+   * Init isotope layout and filters
+   */
+  document.querySelectorAll(".isotope-layout").forEach(function (isotopeItem) {
+    let layout = isotopeItem.getAttribute("data-layout") ?? "masonry";
+    let filter = isotopeItem.getAttribute("data-default-filter") ?? "*";
+    let sort = isotopeItem.getAttribute("data-sort") ?? "original-order";
+
+    let initIsotope;
+    imagesLoaded(isotopeItem.querySelector(".isotope-container"), function () {
+      initIsotope = new Isotope(
+        isotopeItem.querySelector(".isotope-container"),
+        {
+          itemSelector: ".isotope-item",
+          layoutMode: layout,
+          filter: filter,
+          sortBy: sort,
+        }
+      );
+    });
+
+    isotopeItem
+      .querySelectorAll(".isotope-filters li")
+      .forEach(function (filters) {
+        filters.addEventListener(
+          "click",
+          function () {
+            isotopeItem
+              .querySelector(".isotope-filters .filter-active")
+              .classList.remove("filter-active");
+            this.classList.add("filter-active");
+            initIsotope.arrange({
+              filter: this.getAttribute("data-filter"),
+            });
+            if (typeof aosInit === "function") {
+              aosInit();
+            }
+          },
+          false
+        );
+      });
+  });
+
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+})();
+
+/**
+ * Display Menu
+ */
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  toggle.addEventListener("click", () => {
+    // Add show-menu class to nav menu
+    nav.classList.toggle("show-menu");
+    // Add show-icon to show and hide menu icon
+    toggle.classList.toggle("show-icon");
+  });
+};
+showMenu("nav-toggle", "nav-menu");
+
+/**
+ * Display dropdown menu
+ */
+const dropdownItems = document.querySelectorAll(".dropdown__item");
+// 1. Select each dropdown item
+dropdownItems.forEach((item) => {
+  const dropdownButton = item.querySelector(".dropdown__button");
+  // 2. Select each button click
+  dropdownButton.addEventListener("click", () => {
+    // 7. Select the current show-dropdown class
+    const showDropdown = document.querySelector(".show-dropdown");
+    // 5. Call the toggleItem function
+    toggleItem(item);
+    // 8. Remove the show-dropdown class from other items
+    if (showDropdown && showDropdown !== item) {
+      toggleItem(showDropdown);
+    }
+  });
+});
+// 3. Create a function to display the dropdown
+const toggleItem = (item) => {
+  // 3.1. Select each dropdown content
+  const dropdownContainer = item.querySelector(".dropdown__container");
+  // 6. If the same item contains the show-dropdown class, remove
+  if (item.classList.contains("show-dropdown")) {
+    dropdownContainer.removeAttribute("style");
+    item.classList.remove("show-dropdown");
+  } else {
+    // 4. Add the maximum height to the dropdown content and add the show-dropdown class
+    dropdownContainer.style.height = dropdownContainer.scrollHeight + "px";
+    item.classList.add("show-dropdown");
+  }
+};
+
+/**
+ * Remove Dropdown Styles
+ */
+const mediaQuery = matchMedia("(min-width: 1118px)"),
+  dropdownContainer = document.querySelectorAll(".dropdown__container");
+
+// Function to remove dropdown styles in mobile mode when browser resizes
+const removeStyle = () => {
+  // Validate if the media query reaches 1118px
+  if (mediaQuery.matches) {
+    // Remove the dropdown container height style
+    dropdownContainer.forEach((e) => {
+      e.removeAttribute("style");
+    });
+
+    // Remove the show-dropdown class from dropdown item
+    dropdownItems.forEach((e) => {
+      e.classList.remove("show-dropdown");
+    });
+  }
+};
+
+addEventListener("resize", removeStyle);
+
+(function ($) {
+  "use strict";
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
+
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
+})(jQuery);
